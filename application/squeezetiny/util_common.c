a6eb6a08a0967740739f4cd87bc050a0c84ea170
philippe44 <philippe_44@outlook.com>
2018-04-14 11:08:40 -0700

    0.3.0.2

--- application/squeezetiny/util_common.c
+++ application/squeezetiny/util_common.c
@@ -65,11 +65,98 @@
 extern log_level 	util_loglevel;
 //static log_level 	*loglevel = &util_loglevel;
 
+ /* DLNA.ORG_CI: conversion indicator parameter (integer)
+ *     0 not transcoded
+ *     1 transcoded
+ */
+typedef enum {
+  DLNA_ORG_CONVERSION_NONE = 0,
+  DLNA_ORG_CONVERSION_TRANSCODED = 1,
+} dlna_org_conversion_t;
+
+/* DLNA.ORG_OP: operations parameter (string)
+ *     "00" (or "0") neither time seek range nor range supported
+ *     "01" range supported
+ *     "10" time seek range supported
+ *     "11" both time seek range and range supported
+ */
+typedef enum {
+  DLNA_ORG_OPERATION_NONE                  = 0x00,
+  DLNA_ORG_OPERATION_RANGE                 = 0x01,
+  DLNA_ORG_OPERATION_TIMESEEK              = 0x10,
+} dlna_org_operation_t;
+
+/* DLNA.ORG_FLAGS, padded with 24 trailing 0s
+ *     8000 0000  31  senderPaced
+ *     4000 0000  30  lsopTimeBasedSeekSupported
+ *     2000 0000  29  lsopByteBasedSeekSupported
+ *     1000 0000  28  playcontainerSupported
+ *      800 0000  27  s0IncreasingSupported
+ *      400 0000  26  sNIncreasingSupported
+ *      200 0000  25  rtspPauseSupported
+ *      100 0000  24  streamingTransferModeSupported
+ *       80 0000  23  interactiveTransferModeSupported
+ *       40 0000  22  backgroundTransferModeSupported
+ *       20 0000  21  connectionStallingSupported
+ *       10 0000  20  dlnaVersion15Supported
+ *
+ *     Example: (1 << 24) | (1 << 22) | (1 << 21) | (1 << 20)
+ *       DLNA.ORG_FLAGS=0170 0000[0000 0000 0000 0000 0000 0000] // [] show padding
+ */
+typedef enum {
+  DLNA_ORG_FLAG_SENDER_PACED               = (1 << 31),
+  DLNA_ORG_FLAG_TIME_BASED_SEEK            = (1 << 30),
+  DLNA_ORG_FLAG_BYTE_BASED_SEEK            = (1 << 29),
+  DLNA_ORG_FLAG_PLAY_CONTAINER             = (1 << 28),
+
+  DLNA_ORG_FLAG_S0_INCREASE                = (1 << 27),
+  DLNA_ORG_FLAG_SN_INCREASE                = (1 << 26),
+  DLNA_ORG_FLAG_RTSP_PAUSE                 = (1 << 25),
+  DLNA_ORG_FLAG_STREAMING_TRANSFERT_MODE    = (1 << 24),
+
+  DLNA_ORG_FLAG_INTERACTIVE_TRANSFERT_MODE = (1 << 23),
+  DLNA_ORG_FLAG_BACKGROUND_TRANSFERT_MODE  = (1 << 22),
+  DLNA_ORG_FLAG_CONNECTION_STALL           = (1 << 21),
+  DLNA_ORG_FLAG_DLNA_V15                   = (1 << 20),
+} dlna_org_flags_t;
+
+
+
+#define DLNA_ORG_OP (DLNA_ORG_OPERATION_RANGE)
+
+// GNU pre-processor seems to be confused if this is multiline ...
+
+#define DLNA_ORG_FLAG ( DLNA_ORG_FLAG_S0_INCREASE | DLNA_ORG_FLAG_STREAMING_TRANSFERT_MODE | DLNA_ORG_FLAG_BACKGROUND_TRANSFERT_MODE | DLNA_ORG_FLAG_CONNECTION_STALL | DLNA_ORG_FLAG_DLNA_V15 )
+
+
 /*----------------------------------------------------------------------------*/
 /* 																			  */
 /* CODEC & MIME management													  */
 /* 																			  */
 /*----------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+char *make_dlna_content(char *mimetype, u32_t duration) {
+	char *buf;
+	char *DLNAOrgPN;
+
+	switch (mimetype2format(mimetype)) {
+	case 'm':
+		DLNAOrgPN = "DLNA.ORG_PN=MP3;";
+		break;
+	case 'p':
+		DLNAOrgPN = "DLNA.ORG_PN=LPCM;";
+		break;
+	default:
+		DLNAOrgPN = "";
+	}
+
+	asprintf(&buf, "%sDLNA.ORG_OP=00;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=%08x000000000000000000000000",
+						   DLNAOrgPN, DLNA_ORG_FLAG | (duration ? 0 : DLNA_ORG_FLAG_SN_INCREASE));
+
+	return buf;
+}
+
 
 /*---------------------------------------------------------------------------*/
 char *mimetype2ext(char *mimetype)
@@ -156,29 +243,21 @@ char* find_pcm_mimetype(u8_t endian, u8_t *sample_size, bool truncable, u32_t sa
 		if (sscanf(options, "%[^,]", fmt) <= 0) return NULL;
 
 		while (strstr(fmt, "raw")) {
-			char **p, *s;
+			char **p, a[16], r[16], c[16];
 
-			// try the full mime-type
+			// find audio/Lxx
 			p = mimetypes;
-			asprintf(&s, "audio/L%hhu;rate=%u;channels=%hhu", *sample_size, sample_rate, channels);
+			sprintf(a, "audio/L%hhu", *sample_size);
+			sprintf(r, "rate=%u", sample_rate);
+			sprintf(c, "channels=%hhu", channels);
 			while (*p) {
-				if (!strcmp(s, *p)) return strdup(*p);
-				p++;
-			}
-
-			// no luck, try a simple audio/Lxx w/o channel or rate indication
-			p = mimetypes;
-			sprintf(s, "audio/L%hhu", *sample_size);
-			while (*p) {
-				if (!strcmp(s, *p)) {
-					free(s);
-					asprintf(&s, "%s;rate=%u;channels=%hhu", *p, sample_rate, channels);
-					return s;
+				if (strstr(*p, a) &&
+				   (!strstr(*p, "rate=") || strstr(*p, r)) &&
+				   (!strstr(*p, "channels=") || strstr(*p, c))) {
+					return strdup(*p);
 				}
 				p++;
 			}
-
-			free(s);
 
 			if (*sample_size == 24 && truncable) *sample_size = 16;
 			else {
